eb91000159d03349e2cc5016bb5636231ab2a4b1
Matt Berridge <hi@builtbyboon.com>
2015-06-30 00:38:00 +0200

    add iso testing route and create isomorphic rendering component

--- app/client.js
+++ app/client.js
@@ -1,13 +1,20 @@
 var React = require('react');
 
-var Iso = require('higher-order/iso');
 var Demo = require('demo/article');
+var Container = require('components/container');
 
-var isos = document.querySelectorAll('.az-iso-item');
+var allComponents = {
+  demo: Demo,
+  container: Container
+}
+
+var isos = document.querySelectorAll('.az-iso');
 
 for (var i = 0, len = isos.length; i < len; i++) {
   var iso = isos[i];
-  // var data = iso.querySelector('.az-iso-data').getAttribute('data-az-iso');
-  var el = React.createElement(Demo);
-  React.render(el, iso);
+  var wrapper = iso.querySelector('.az-iso-component');
+  var component = JSON.parse(iso.getAttribute('data-meta')).component;
+  var state = JSON.parse(iso.querySelector('.az-iso-state').getAttribute('data-state'));
+  var el = React.createElement(allComponents[component], state);
+  React.render(el, wrapper);
 }
--- server/middleware/isoTestingRoute.js
+++ server/middleware/isoTestingRoute.js
@@ -1,20 +1,26 @@
 var React = require('react');
 
 var Iso = require('../../app/node_modules/higher-order/iso');
-var Demo = require('../../app/node_modules/demo/article');
+var Container = require('../../app/node_modules/components/container');
 
 module.exports = function(req, res) {
   /*
    * A dummy function which does the following:
    *  - Get the article from the API for given 'articleId'
-   *  - Renders an Article component.
+   *  - Renders an Article componet.
    */
   // articleId is resolved by 'middleware/routingParams.js'. Therefore, the
   // response of the "Article API endpoint" is on req.item.
   // The actual article data is in req.item.data
+
   var iso = new Iso();
-  var articleData = req.item.data;
-  var element = React.createElement(Demo, articleData);
-  var isoWrapped = iso.render(React.renderToString(element), articleData, { component: 'demo' });
-  res.send(isoWrapped);
+  var data = {
+    title: 'Testing1234'
+  };
+  var element = React.createElement(Container, data);
+  // res.send();
+  res.render('iso.html', {
+    title: 'Iso Rendering',
+    content: iso.render(React.renderToString(element), data, { component: 'container' })
+  });
 };
