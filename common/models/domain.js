7c8f66899f0285a57c4e9892e47d26c58c10bfb2
Florian Hämmerle <haemmerle.florian@outlook.com>
2015-06-30 12:42:02 +0200

    move functionality to model `Domain`

--- common/models/domain.js
+++ common/models/domain.js
@@ -1,3 +1,17 @@
 module.exports = function(Domain) {
-
+  Domain.fromRequest = function(req, cb) {
+    if (!req.domainConfig) {
+      var domainId = req.get('X_ZOPE_SKIN') || 'aaz';
+      Domain.get(domainId, function(err, domain) {
+        if (err) {
+          cb(err, undefined);
+        } else {
+          req.domainConfig = domain;
+          cb(null, req.domainConfig);
+        }
+      });
+    } else {
+      cb(null, req.domainConfig);
+    }
+  }
 };
