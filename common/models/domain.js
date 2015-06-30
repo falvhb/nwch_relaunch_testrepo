b8f4fcaf37ca5a919d2c2b67fc419ec8e184411e
Florian Hämmerle <haemmerle.florian@outlook.com>
2015-06-30 13:49:53 +0200

    add comment and remove redundant `undefined` parameter from callback

--- common/models/domain.js
+++ common/models/domain.js
@@ -1,10 +1,12 @@
 module.exports = function(Domain) {
+  // loads the domain configuration via the API if it wasn't already loaded
+  // this method stores the retrieved domain configuration in the request
   Domain.fromRequest = function(req, cb) {
     if (!req.domainConfig) {
       var domainId = req.get('X_ZOPE_SKIN') || 'aaz';
       Domain.get(domainId, function(err, domain) {
         if (err) {
-          cb(err, undefined);
+          cb(err);
         } else {
           req.domainConfig = domain;
           cb(null, req.domainConfig);
