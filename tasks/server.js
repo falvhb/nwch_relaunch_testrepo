cce9144785ba9499f8ca00dafe3410bfd6dfc024
Richard Bausek <r.bausek@de.edenspiekermann.com>
2015-07-07 00:23:25 +0200

    update nodemon settings
    only listen to app specific file (jsx js json)
    
    TODO: make nodemon listen to files starting with a "." like .data.json

--- tasks/server.js
+++ tasks/server.js
@@ -6,9 +6,12 @@ var helpers = require('./lib/helpers.js');
 module.exports = function() {
   nodemon({
     script: pkg.main,
+    // @TODO: move all API keys into .env file – don't keep them in Git
     env: {
       'NODE_ENV': (helpers.isProd ? 'production' : 'development'),
       'DISQUS_API_KEY': '???'
-    }
+    },
+    watch: ['app/', 'app/node_modules', 'common/', 'server/'],
+    ext: 'jsx js json'
   });
 };
