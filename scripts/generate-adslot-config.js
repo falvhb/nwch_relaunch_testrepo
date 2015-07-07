f5597238431a229b497c89031fd55c3f3af9a09e
Richard Bausek <r.bausek@de.edenspiekermann.com>
2015-07-07 17:20:32 +0200

    change structure of ad config

--- scripts/generate-adslot-config.js
+++ scripts/generate-adslot-config.js
@@ -1,3 +1,6 @@
+// generates a JSON file based on CSV output from
+// https://docs.google.com/spreadsheets/d/1ZmNbq_A75hE9RnaCurP3jqwoUrxONv9DZSf4sF9TWQU/edit?usp=sharing
+
 var fs = require('fs');
 var csv = require('ya-csv');
 var _ = require('lodash');
@@ -32,9 +35,14 @@ var processAdSlotConfig = function (data) {
   return filteredData;
 };
 
-var getAdSlotsBySite = function (siteId) {
+var getAdSlotsByWebsiteId = function (siteId) {
   return _.where(adSlots, { 'website_id': siteId});
 };
+
+var getAdSlotsBySiteId = function (collection, siteId) {
+  return _.where(collection, { 'site_id': siteId});
+};
+
 
 var getAdSlotsByPlacementSlotId = function (collection, placementSlotId) {
   return _.where(collection, { 'placement_slot_id': placementSlotId});
@@ -44,7 +52,8 @@ var getAdSlotsByPlacementSlotId = function (collection, placementSlotId) {
 var getBoundsforAdSlot = function (slot) {
   var typeRegExp = /(Mobile|Tablet|Desktop)$/i;
   var matches = typeRegExp.exec(slot.placement_name);
-  var bounds = {};
+  var bounds = { id: slot.placement_id, min: 729, max: 9999 };
+
   if(matches && matches.length) {
     var type = matches[0];
     switch (type) {
@@ -62,32 +71,49 @@ var getBoundsforAdSlot = function (slot) {
 }
 
 var parseSiteSlots = function (siteSlots) {
-  var placementSlotIds = _.unique(_.pluck(siteSlots, 'placement_slot_id'));
+  var siteIds = _.unique(_.pluck(siteSlots, 'site_id'));
+  console.log(siteIds);
 
-  // loop through all placement slot ids and create config
-  var siteSlotPlacements = _.map(placementSlotIds, function (placementSlotId) {
-    var placement = {
-      name: placementSlotId
-    };
-    var placementSlots = getAdSlotsByPlacementSlotId(siteSlots, placementSlotId);
+  // loop through all availble site_id of current site
+  var sitePageTypes = _.map(siteIds, function (siteId) {
+    var slots = getAdSlotsBySiteId(siteSlots, siteId);
+    var placementSlotIds = _.unique(_.pluck(slots, 'placement_slot_id'));
 
-    placement.slots = placementSlots;
-    placement.config = {
-      responsive: {
-        useresponsive: true,
-        bounds: []
-      },
-      params: { target: '_blank' }
-    };
+    // loop through all placement slot ids and create config
+    var siteTypePlacements = _.map(placementSlotIds, function (placementSlotId) {
+      var placement = {
+        name: placementSlotId
+      };
+      var placementSlots = getAdSlotsByPlacementSlotId(slots, placementSlotId);
 
-    // create the bounds for the placement
-    var bounds = _.map(placementSlots, getBoundsforAdSlot);
-    placement.config.responsive.bounds = bounds;
+      // placementSlots = _.map(placementSlots, function (slot) {
+      //   return _.omit(slot, ['site_id', 'site_name']);
+      // });
 
-    return placement;
+      placement.slots = placementSlots;
+      placement.config = {
+        responsive: {
+          useresponsive: true,
+          bounds: []
+        },
+        params: { target: '_blank' }
+      };
+
+      // create the bounds for the placement
+      var bounds = _.map(placementSlots, getBoundsforAdSlot);
+      placement.config.responsive.bounds = bounds;
+
+      return placement;
+    });
+
+    return {
+      "site_name": slots[0].site_name,
+      "site_id": siteId,
+      "placements": siteTypePlacements
+    }
   });
 
-  return siteSlotPlacements;
+  return sitePageTypes;
 }
 
 
@@ -95,11 +121,11 @@ var parseAdSlots = function (adSlots) {
   var siteIds = _.unique(_.pluck(adSlots, 'website_id'));
 
   var allSiteSlots = _.map(siteIds, function (siteId) {
-    var siteSlots = getAdSlotsBySite(siteId);
+    var siteSlots = getAdSlotsByWebsiteId(siteId);
     var siteName = siteSlots[0].website_name;
     var output = {
-      siteName: siteName,
-      siteId: siteId
+      website_name: siteName,
+      website_id: siteId
     }
 
     siteSlots = _.map(siteSlots, function (slot) {
@@ -109,7 +135,7 @@ var parseAdSlots = function (adSlots) {
     // parse the siteSlots to group the placements
     siteSlots = parseSiteSlots(siteSlots);
 
-    output.placements = siteSlots;
+    output.page_types = siteSlots;
     return output;
   });
   return allSiteSlots;
