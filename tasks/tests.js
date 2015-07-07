5ab613ecc451fa131bbf8a2d008f436c63c07484
Lukas Ender <hello@lukasender.at>
2015-07-07 18:20:45 +0200

    tests: return error code.

--- tasks/tests.js
+++ tasks/tests.js
@@ -1,20 +1,30 @@
-var gulp = require('gulp');
 var karma = require('karma').server;
 var path = require('path');
 
+// API tests
+
+function apiTests(done) {
+  var mockaRunner = require('child_process').spawn('npm', ['run', 'mocha']);
+  mockaRunner.stdout.pipe(process.stdout);
+  mockaRunner.stderr.pipe(process.stderr);
+  mockaRunner.on('exit', function(code) {
+    var result = code > 0 ? code : null;
+    done(result);
+  });
+}
+
+// React tests
+
+var karmaOptions = {
+  configFile: path.join(__dirname, '../karma.conf.js'),
+  singleRun: true
+};
+
+function reactTests(done) {
+  return karma.start(karmaOptions, done);
+}
+
 module.exports = {
-  api: function(done) {
-    var mockaRunner = require('child_process').spawn('npm', ['run', 'mocha']);
-    mockaRunner.stdout.pipe(process.stdout);
-    mockaRunner.stderr.pipe(process.stderr);
-    mockaRunner.on('exit', function(code) {
-      done();
-    });
-  },
-  react: function(done) {
-    karma.start({
-      configFile: path.join(__dirname, '../karma.conf.js'),
-      singleRun: true
-    }, done);
-  }
+  api: apiTests,
+  react: reactTests
 };
