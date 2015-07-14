7cce5496728a961fcac8a4797a4468b5957cd0a3
Matt Berridge <hi@builtbyboon.com>
2015-07-14 12:29:33 +0200

    rehydrate missing components

--- app/client.js
+++ app/client.js
@@ -1,10 +1,14 @@
 var Iso = require('iso-react');
 var iso = new Iso();
 
+var ArticleHeader = require('components/article-header');
+var ArticleIntro = require('components/article-intro');
 var ArticleDetail = require('components/article-detail');
 var Teaser = require('components/teaser');
 
 var components = {
+  articleHeader: ArticleHeader,
+  articleIntro: ArticleIntro,
   articleDetail: ArticleDetail,
   teaser: Teaser
 };
