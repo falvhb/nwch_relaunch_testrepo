a9b06e41e924b816a5d7eaf01831a4fcfa500ad9
Matt Berridge <hi@builtbyboon.com>
2015-07-28 12:35:49 +0200

    clean up env vars

--- tasks/server.js
+++ tasks/server.js
@@ -3,18 +3,10 @@ var pkg = require('../package.json');
 var helpers = require('./lib/helpers');
 
 // load all env variables
-var envVars = helpers.ENV_VARS;
 var envPath = helpers.isProduction() ? '.env' : '.env.dev';
 require('dotenv').load({
   path: envPath
 });
-
-// put them into an object for nodemon
-var envObj = {};
-for (var i = 0; i < envVars.length; ++i) {
-  var key = envVars[i];
-  envObj[key] = process.env[key];
-}
 
 module.exports = function() {
   nodemon({
