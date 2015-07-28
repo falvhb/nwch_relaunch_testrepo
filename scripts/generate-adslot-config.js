767f333189dfcea23a995596032c7455a8bd7983
Richard Bausek <r.bausek@de.edenspiekermann.com>
2015-07-28 11:44:30 +0200

    WIP refactor ad implementation

--- scripts/generate-adslot-config.js
+++ scripts/generate-adslot-config.js
@@ -4,62 +4,97 @@
 var fs = require('fs');
 var csv = require('ya-csv');
 var _ = require('lodash');
-var toSlug = require('to-slug')
+var toSlug = require('to-slug');
+var skins = require('../app/node_modules/config/skins.json');
+var outputPath = '../client/config/advertising/';
 
 var importFile = './az-banners-all-sites.csv';
+var csvColumnNames = [
+  'websiteId',
+  'websiteName',
+  'categoryName',
+  'categoryComment',
+  'siteId',
+  'siteName',
+  'placementId',
+  'placementName',
+  'placementSize',
+  'placementPosition',
+  'placementType',
+  'placementStatus',
+  'commissionCPM',
+  'commissionType',
+  'commissionCPC',
+  'commissionType2',
+  'contactName',
+  'contactEmail',
+  'bannerSizeId',
+  'alias',
+  'extId',
+  'mediatypeId',
+  'placementClass'
+];
 
 var adPlacements = [];
 
-function save(fileName, data) {
-  fs.writeFile(fileName, data, function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      console.log("The file %s was saved", fileName);
-    }
+
+// skin helpers
+
+function getSkinFromDomain (domain) {
+  var result = _.findKey(skins, function (item) {
+    return item === domain;
   });
+  return result;
 }
 
+
+
+/**
+ * processes each raw CSV row object
+ *
+ * @param  {Object}  data  Object containing a row of CSV data
+ *
+ * @return  {Object}  The processed object
+ */
 var processAdPlacementConfig = function (data) {
   var filteredData = _.pick(data, [
-      'website_id',
-      'website_name',
-      'site_id',
-      'site_name',
-      'placement_id',
-      'placement_name',
-      // 'placement_size',
-      // 'placement_position',
+      'websiteId',
+      'websiteName',
+      'siteId',
+      'siteName',
+      'placementId',
+      'placementName',
       'placement_slot_id'
   ]);
 
   var sizeType = getSizeTypeForPlacement(filteredData);
   var slotName = getSlotNameForPlacement(filteredData);
 
-  filteredData.placement_type = sizeType;
-  filteredData.slot_name = slotName;
+  filteredData.placementType = sizeType;
+  filteredData.slotName = slotName;
 
   return filteredData;
 };
 
 var getAdPlacementsByWebsiteId = function (collection, siteId) {
-  return _.where(collection, { 'website_id': siteId});
+  return _.where(collection, { 'websiteId': siteId});
 };
+
 
 var getAdSlotsBySiteId = function (collection, siteId) {
-  return _.where(collection, { 'site_id': siteId});
+  return _.where(collection, { 'siteId': siteId});
 };
 
 
-var getAdSlotsByPlacementSlotId = function (collection, placementSlotId) {
-  return _.where(collection, { 'placement_slot_id': placementSlotId});
+var getAdSlotsBySlotName = function (collection, placementSlotId) {
+  return _.where(collection, { 'slotName': placementSlotId});
 };
 
 
 var getSizeTypeForPlacement = function (placement) {
   var typeRegExp = /(Mobile|Tablet|Desktop)$/i;
-  var matches = typeRegExp.exec(placement.placement_name);
-  var bounds = { id: placement.placement_id, min: 729, max: 9999 };
+  var matches = typeRegExp.exec(placement.placementName);
+  var bounds = { id: placement.placementId, min: 729, max: 9999 };
   var sizeType = 'desktop';
 
   if (matches && matches.length) {
@@ -70,41 +105,44 @@ var getSizeTypeForPlacement = function (placement) {
   return sizeType;
 };
 
+
 var getSlotNameForPlacement = function (placement) {
   var regex = /^(Artikel|Static|Home|Ressort) ([a-zA-Z0-9\/\s]+?) (Mobile|Tablet|Desktop|Native)$/ig;
-  var matches = regex.exec(placement.placement_name);
+  var matches = regex.exec(placement.placementName);
 
   if (matches && matches.length) {
     var pageType = matches[0];
     var slot = matches[2];
     return toSlug(slot.replace("/", "_"));
+  } else {
+    throw new Error('Naming of placement is not matching schema', placement);
   }
 }
 
 var parseSiteSlots = function (siteSlots) {
-  var siteIds = _.unique(_.pluck(siteSlots, 'site_id'));
+  var siteIds = _.unique(_.pluck(siteSlots, 'siteId'));
   console.log(siteIds);
 
-  // loop through all availble site_id of current site
+  // loop through all availble siteId of current site
   var sitePageTypes = _.map(siteIds, function (siteId) {
     var slots = getAdSlotsBySiteId(siteSlots, siteId);
-    var placementSlotIds = _.unique(_.pluck(slots, 'placement_slot_id'));
+    var slotNames = _.unique(_.pluck(slots, 'slotName'));
 
     // loop through all placement slot ids and create config
-    var siteTypeSlots = _.map(placementSlotIds, function (placementSlotId) {
+    var siteTypeSlots = _.map(slotNames, function (slotName) {
       var pageTypeRegex = /^(Artikel|Static|Home|Ressort)_/ig;
-      var slotName = placementSlotId.replace(pageTypeRegex, '');
+      var slotName = slotName.replace(pageTypeRegex, '');
       var slot = {
         name: toSlug(slotName)
       };
-      var placementSlots = getAdSlotsByPlacementSlotId(slots, placementSlotId);
+      var placementSlots = getAdSlotsBySlotName(slots, slotName);
 
       // omit non needed attributes
       placementSlots = _.map(placementSlots, function (placement) {
         var placementConfig = {
-          id: placement.placement_id,
-          name: placement.placement_name,
-          type: placement.placement_type
+          id: placement.placementId,
+          name: placement.placementName,
+          type: placement.placementType
         }
 
         return placementConfig;
@@ -116,8 +154,8 @@ var parseSiteSlots = function (siteSlots) {
     });
 
     return {
-      "site_name": slots[0].site_name,
-      "site_id": siteId,
+      "siteName": slots[0].siteName,
+      "siteId": siteId,
       "slots": siteTypeSlots
     }
   });
@@ -127,48 +165,89 @@ var parseSiteSlots = function (siteSlots) {
 
 
 var parseAdPlacements = function (adSlots) {
-  var siteIds = _.unique(_.pluck(adSlots, 'website_id'));
+  var siteIds = _.unique(_.pluck(adSlots, 'websiteId'));
 
   var allSiteSlots = _.map(siteIds, function (siteId) {
     var siteSlots = getAdPlacementsByWebsiteId(adSlots, siteId);
-    var siteName = siteSlots[0].website_name;
+    var siteName = siteSlots[0].websiteName;
     var siteDomain = siteName.replace(/_([0-9]{4})/, '');
     var output = {
-      website_name: siteName,
-      website_domain: siteDomain,
-      website_id: siteId
+      websiteName: siteName,
+      websiteDomain: siteDomain,
+      websiteId: siteId
     }
 
     siteSlots = _.map(siteSlots, function (slot) {
-      return _.omit(slot, ['website_id', 'website_name']);
+      return _.omit(slot, ['websiteId', 'websiteName']);
     });
 
     // parse the siteSlots to group the placements
     siteSlots = parseSiteSlots(siteSlots);
 
-    output.page_types = siteSlots;
+    output.pageTypes = siteSlots;
+
     return output;
   });
   return allSiteSlots;
+};
+
+
+// Saving functions
+
+
+function save(fileName, data) {
+  fs.writeFile(fileName, data, function(err) {
+    if(err) {
+      throw new Error('error saving file', err);
+    } else {
+      console.log("The file %s was saved", fileName);
+    }
+  });
 }
 
+
+var saveFiles = function (adConfig) {
+  // save the main config
+  var output = JSON.stringify(adConfig, undefined, 2);
+  save('../app/node_modules/advertising/config/ad-config.json', output);
+
+  // save by domain and page type
+  _.each(adConfig, function (siteConfig) {
+    var skin = siteConfig.websiteName;
+    var output = JSON.stringify(siteConfig, undefined, 2);
+    //save(outputPath + skin + '.json', output);
+  });
+}
+
+
+// CSV parsing
+
 var readCSV = function () {
-  var reader = csv.createCsvFileReader(importFile, {
-    columnsFromHeader: true
-    }
-  );
+  var parseOptions = {
+    'separator': ';',
+    'quote': '"',
+    'escape': '"',
+    'comment': '',
+  };
+  var reader = csv.createCsvFileReader(importFile, parseOptions);
+  reader.setColumnNames(csvColumnNames);
 
   reader.addListener('data', function(data) {
+    // skip the column names row if present
+    if(adPlacements.length === 0 && data.websiteId === "Website ID") {
+      return;
+    }
     data = processAdPlacementConfig(data);
     adPlacements.push(data);
   });
 
   reader.addListener('end', function() {
     console.log('Processing %s entries', adPlacements.length);
-    allSiteSlots = parseAdPlacements(adPlacements);
-    var output = JSON.stringify(allSiteSlots, undefined, 2);
-    save('../app/node_modules/advertising/config/ad-config.json', output);
+    var adConfig = parseAdPlacements(adPlacements);
+    saveFiles(adConfig);
+
   });
 };
 
+// start parsing
 readCSV();
