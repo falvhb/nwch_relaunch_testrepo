4491c37ac3ca75ba2d56455f5d5b9e1ecb40ff29
Florian Hämmerle <haemmerle.florian@outlook.com>
2015-07-29 10:50:29 +0200

    clean up

--- server/routing/routingDossierHeader.js
+++ server/routing/routingDossierHeader.js
@@ -5,68 +5,81 @@ module.exports = function(req, res) {
   var params = req.params;
 
   var componentName = 'dossier-header';
-  if (!componentName) {
-    res.send('<!-- No component name provided! -->');
-    return;
-  }
+
   var componentVariation = params.variation;
   if (!componentVariation) {
     res.send('<!-- No variation name provided! -->');
     return;
   }
+
+  // resolve the component
   var component;
   try {
-    component = require('../../app/node_modules/components/' +componentName);
+    component = require('../../app/node_modules/components/' + componentName);
   } catch (e) {
-    // uncomment these lines when replacing the dummy component below with an actual component
+    //TODO: uncomment these lines when replacing the dummy component with a real component
     //res.send('<!-- Component "' + componentName + '" not found! -->');
     //return;
   }
-  // var slot;
-  // try {
-  //   slot = require('../../app/node_modules/components/' + componentName + '/slot');
-  // } catch (e) {
-  //   // not found (is okay, continue)
-  // }
 
-  // just a dummy component to render - remove when actual component is ready
+  // see if there is a slot function
+  // this typically maps data from the full API response -> only the data a component needs
+  var slot;
+  try {
+    slot = require('../../app/node_modules/components/' + componentName + '/slot');
+  } catch (e) {
+    // not found (is okay, continue)
+  }
+
+  // just a dummy component - remove when actual component is ready
   component = React.createClass({
     displayName: 'RessortHeaderDummy',
 
     render: function() {
-      if (!this.props.data.dossier.data[0]) {
+      if (!this.props.data.dossier) {
         return React.createElement('div', {}, 'Dossier not found!');
       }
       return React.createElement('div', {},
-        React.createElement('h1', {}, this.props.data.dossier.data[0].title),
-        React.createElement('p', {}, this.props.data.dossier.data[0].header.lead)
+        React.createElement('h1', {}, this.props.data.dossier.title),
+        React.createElement('p', {}, this.props.data.dossier.header.lead),
+        React.createElement('div', {}, 'Keywords'),
+        React.createElement('ul', {},
+          this.props.data.dossier.keywords.map(function(keyword) {
+            return React.createElement('li', {}, keyword);
+          })
+        )
       );
     }
   });
 
 
-  // Do what needs to be done to render the component.
   function render() {
+    var result = req.item || {};
+    var dossier = result && result.data ? result.data[0] : null;
+
     var state = {
-      'dossier': req.item,
+      'dossier': dossier,
       'variation': componentVariation
     };
 
-    var element = React.createElement(component, { data: state });
-
-    res.send(React.renderToString(element));
+    res.send(React.renderToString(React.createElement(component, { data: state })));
   }
 
-  // Fetch the dossier and call render() afterwards.
+  // do the dossier query
+  var queryParams = {
+    'path': 'dossier-container/' + req.params.dossier
+  };
+
   req.app.models.NewsRessort.query(
-    { path: 'dossier-container/' + req.params.dossier },
+    queryParams,
     function(err, result) {
       if (err) {
         req.item = null;
       } else {
+        // got a result to render
         req.item = result;
       }
       render();
     }
-  );
+    );
 };
