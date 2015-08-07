4fb90b02e87c411346511ebc9482e64a8264dfe6
Matt Berridge <hi@builtbyboon.com>
2015-08-07 14:31:22 +0200

    re-enable advertising

--- app/client.js
+++ app/client.js
@@ -11,8 +11,7 @@ var Footer = require('components/footer');
 var TopicDetail = require('components/topic-detail');
 var HeaderNav = require('components/header-nav');
 
-// advertising = Advertising.init('.ad');
-
+advertising = Advertising.init('.ad');
 
 var components = {
   articleHeader: ArticleHeader,
