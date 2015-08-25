449f666f801113136a72ad3e05fa03b5b2516ef3
Matt Berridge <hi@builtbyboon.com>
2015-08-25 18:04:07 +0200

    Tweak clear empty cells helper to accept more instances

--- app/client.js
+++ app/client.js
@@ -48,4 +48,6 @@ recommendationsTop.init();
 
 var ClearEmptyCells = require('clientside/clear-empty-cells');
 var clearEmptyCells = new ClearEmptyCells();
-clearEmptyCells.init();
+clearEmptyCells.init({ selector: '.grid__item' });
+clearEmptyCells.init({ selector: '.recommendations-top__cell' });
+clearEmptyCells.init({ selector: '.recommendations-bottom__cell' });
