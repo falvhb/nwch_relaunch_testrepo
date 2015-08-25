0e037f38ab0e0399df4102f9169cc5fabf671526
Richard Bausek <r.bausek@de.edenspiekermann.com>
2015-08-26 01:34:11 +0200

    add Function.bind polyfill to make PhantomJS happy

--- app/vendor.js
+++ app/vendor.js
@@ -1,3 +1,5 @@
+// bind polyfill for Phantomjs and older browsers
+require('polyfill-function-prototype-bind');
 require('svg4everybody/svg4everybody');
 var Flickity = require('flickity/dist/flickity.pkgd.js');
 window.Flickity = Flickity;
