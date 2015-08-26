00fd88286f48412bb3be9de070f1409768ef7e48
Matt Berridge <hi@builtbyboon.com>
2015-08-26 16:50:16 +0200

    allow conditional slot JS logic for cleaner markup and client bundle

--- server/helpers/renderComponent.js
+++ server/helpers/renderComponent.js
@@ -1,20 +1,37 @@
+var React = require('react');
 var camelCase = require('camelcase');
 var Iso = require('../../app/node_modules/iso-react');
 
 module.exports = function renderComponent(options) {
-  var component = options.component;
+
+  var componentElement = options.componentElement;
   var componentName = options.componentName;
   var componentVariation = options.componentVariation;
   var state = options.state;
   var slot = options.slot;
-  var iso = new Iso();
-  var isoWrapped = iso.wrap({
-    component: component,
-    state: (slot && typeof(slot.data) === 'function') ? slot.data(state) : state,
-    meta: {
-      id: camelCase(componentName),
-      variation: componentVariation
-    }
-  });
-  return isoWrapped;
+
+  var componentData = (slot && typeof(slot.data) === 'function')
+    ? slot.data(state)
+    : state;
+
+  var output;
+
+  if (slot && slot.iso) {
+    var iso = new Iso();
+
+    output = iso.wrap({
+      component: componentElement,
+      state: componentData,
+      meta: {
+        id: camelCase(componentName),
+        variation: componentVariation
+      }
+    });
+
+  } else {
+    var el = React.createElement(componentElement, componentData);
+    output = React.renderToString(el, componentData);
+  }
+
+  return output;
 };
