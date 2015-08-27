f906b147a85159ce362b2f2fe8b84c9fd72d911b
Matt Berridge <hi@builtbyboon.com>
2015-08-27 11:08:38 +0200

    allow disabling of iso by variation to exclude teasers without images

--- server/helpers/renderComponent.js
+++ server/helpers/renderComponent.js
@@ -1,11 +1,13 @@
 var React = require('react');
+var _ = require('lodash');
 var camelCase = require('camelcase');
+var disabledIsosByVariation = require('./disabledIsosByVariation');
 var Iso = require('../../app/node_modules/iso-react');
 
 module.exports = function renderComponent(options) {
 
   var componentElement = options.componentElement;
-  var componentName = options.componentName;
+  var componentName = camelCase(options.componentName);
   var componentVariation = options.componentVariation;
   var state = options.state;
   var slot = options.slot;
@@ -16,14 +18,14 @@ module.exports = function renderComponent(options) {
 
   var output;
 
-  if (slot && slot.iso) {
+  if (_.get(slot, 'iso') && !_.includes(disabledIsosByVariation[componentName], componentVariation)) {
     var iso = new Iso();
 
     output = iso.wrap({
       component: componentElement,
       state: componentData,
       meta: {
-        id: camelCase(componentName),
+        id: componentName,
         variation: componentVariation
       }
     });
