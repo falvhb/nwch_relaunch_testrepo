af7d4386e169359f1987aafa65230e09200b8c82
Matt Berridge <hi@builtbyboon.com>
2015-08-27 16:09:51 +0200

    RelatedVideos: dont pass a state as the component doesnt need it

--- server/helpers/renderComponent.js
+++ server/helpers/renderComponent.js
@@ -9,9 +9,12 @@ module.exports = function renderComponent(params) {
   var output, component = params || {};
 
   component.name = camelCase(params.name);
-  component.data = (component.slot && typeof(component.slot.data) === 'function')
-    ? component.slot.data(component.state)
-    : component.state;
+
+  if (component.slot && typeof(component.slot.data) === 'function') {
+    component.data = component.slot.data(component.state);
+  } else if (component.slot.data == null) {
+    component.data = component.state;
+  }
 
   if (_.get(component.slot, 'iso') && !_.includes(disabledIsosByVariation[component.name], component.variation)) {
 
