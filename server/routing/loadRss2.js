0d69cd6a6e1eee336fd967a7cb0e19b437ee2443
Lukas Ender <hello@lukasender.at>
2015-08-31 10:22:34 +0200

    RSS2 Feed: adapt to API changes: /rss/<ressort>/<subressort> & use the new ‘days’ filter.

--- server/routing/loadRss2.js
+++ server/routing/loadRss2.js
@@ -2,30 +2,27 @@
 
 var Qs = require('qs');
 var ressortPath = require('../modules/ressortPath');
-var moment = require('moment');
 
-var ITEMS_LIMIT = 20;
+var DAYS_NON_RESSORT = 90;
 
 
 /**
- * A middleware to load the RSS Feed.
+ * A middleware to load data for an RSS Feed.
  */
 function loadRss(req, res, next) {
   var domain = req.headers['x-skin'] || 'aaz';
-  var effective = moment().utc().subtract(10, 'days').format();
   var queryParams = {
-    limit: ITEMS_LIMIT,
-    effective: effective,
+    'domain': domain,
     sort: 'newest'
   };
   var rsPath = ressortPath(req);
-  if (rsPath) {
-    // only set a ressort filter if it is actually required.
-    queryParams.ressort = rsPath;
+  if (!rsPath) {
+    rsPath = '';
+    // limit the number of days because of performance
+    queryParams.days = DAYS_NON_RESSORT;
   }
-
   var apiConfig = {
-    endpoint: '/rss/' + domain + '?' + Qs.stringify(queryParams),
+    endpoint: '/rss/' + rsPath + '?' + Qs.stringify(queryParams),
     key: 'rss2'
   };
 
