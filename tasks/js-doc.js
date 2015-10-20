a6444a2d97db84fb370944fe8503986c5d8fc200
philipp-az <philipp.killer@azmedien.ch>
2015-10-20 10:20:19 +0200

    configured tracking directory as source for jsdoc

--- tasks/js-doc.js
+++ tasks/js-doc.js
@@ -2,12 +2,20 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 
+var options = {
+  source: './app/node_modules/tracking',
+  destination: './docs/jsdoc/app/tracking',
+};
+
 module.exports = function() {
-  return gulp.
-      src('./app/node_modules/tracking/jsdoc-dummy.js')
+  var command = './node_modules/.bin/jsdoc ' + options.source + ' -d ' + options.destination;
+
+  return gulp
+      // Just specify one file as source
+      // The file itself is not used for the task.
+      // We just have to provide one existing source file to run gulp.
+      .src('./gulpfile.js')
       .pipe(
-        shell([
-          './node_modules/.bin/jsdoc ./app/node_modules/tracking/jsdoc-dummy.js -d ./docs/jsdoc/app/tracking'
-        ])
+        shell([command])
       );
 };
