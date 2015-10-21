45e51b9399cb1e63439dd9d82be56f185656d8d9
philipp-az <philipp.killer@azmedien.ch>
2015-10-21 09:53:00 +0200

    standardized net metrix page view tracking for styleguide and website

---
+++ app/assets/scripts/tracking/netmetrix.js
@@ -0,0 +1,23 @@
+if (typeof window.AZTracker !== 'undefined' && window.AZTracker.trackPageView) {
+  window.AZTracker.trackPageView({
+    domain: 'aznetz',
+    path: {
+      product: 'live',
+
+      // exclude as it is already contained in url path
+      // sitename: 'styleguide',
+
+      // component name event tracking, "page" or "empty" for pageview tracking
+      // @TODO: adjust in concept
+      // view: 'page',
+
+      path: window.location.pathname,
+      event: 'pageview',
+      id: null,
+      index: null
+    },
+    referer: document.referer,
+    screenWidth: screen.width,
+    screenHeight: screen.height
+  });
+}
