13e7aa52d060a34a93dbf1f5f0b67f554a256c4e
Matt Berridge <hi@builtbyboon.com>
2015-10-21 11:41:45 +0200

    Ads: use class foe sticky node and tweak skyscraper and other layout

--- app/client.js
+++ app/client.js
@@ -54,14 +54,18 @@ Advertising.init('.ad');
 // Ad Stickiness
 var StickyNode = require('advertising/sticky-node');
 
+var stickyNode = new StickyNode({
+  element: '#side_ad',
+  parent: '.ad--skyscraper',
+  container: '#sticky-wrapper',
+  offset: 170,
+  breakpoint: 1400
+});
+
+window.StickyNode = stickyNode;
+
 var loadFunction = function() {
-  StickyNode.init({
-    element: '#side_ad',
-    parent: '.ad--skyscraper',
-    container: '#sticky-wrapper',
-    offset: 170,
-    breakpoint: 1400
-  });
+  stickyNode.init();
 };
 
 window.addEventListener('load', loadFunction, false);
