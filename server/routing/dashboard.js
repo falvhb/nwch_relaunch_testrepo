0f5610231a3efb6ad4d26f67392da7597687ec03
marcohamersma <hello@marcohamersma.nl>
2015-10-22 11:41:15 +0200

    cleanup duplication

--- server/routing/dashboard.js
+++ server/routing/dashboard.js
@@ -1,17 +1,12 @@
 var getSkinName = require('../modules/skin');
+var getIconPath = require('../modules/icon-path');
 
 module.exports = function(req, res) {
   var skin = getSkinName(req);
-  var iconPath = 'az';
-  if (skin.match(/^(bzb|blz)$/)) {
-    iconPath = 'bz';
-  } else if (skin.match(/^(ot)$/)) {
-    iconPath = 'ot';
-  }
   var version = process.env.VERSION || '@@VERSION';
   res.render('layouts/dashboard.html', {
     staticBasePath: '/__node__/' + version + '/__static__',
-    iconPath: iconPath,
+    iconPath: getIconPath(skin),
     skin: skin
   });
 };
