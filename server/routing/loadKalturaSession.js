d4ca49636a9fdeac439b3121d5488bb937693147
philipp-az <philipp.killer@azmedien.ch>
2015-11-02 13:17:56 +0100

    replaced kaltura session generation using service 'user'

--- server/routing/loadKalturaSession.js
+++ server/routing/loadKalturaSession.js
@@ -4,24 +4,42 @@ var Kaltura = require('kaltura-client/KalturaClient.js');
  * A middleware to load the Kaltura Session.
  */
 module.exports = function(req, res, next) {
-  var accountID = '1789881';
-  var secret = process.env.KALTURA_ADMIN_SECRET;
-  var userId = 'API3rdPartyAdmin';
+  var partnerId = '1789881';
+  var userId = 'Viewer';
+  var password = 'Lfd7sdfhl!@';
   var expiry = 86400;
   var privileges;
 
-  var config = new Kaltura.KalturaConfiguration(accountID);
+  var config = new Kaltura.KalturaConfiguration(partnerId);
   var client = new Kaltura.KalturaClient(config);
-  var type = Kaltura.enums.KalturaSessionType.ADMIN;
-  client.session.start(
-    function (ks) {
+
+  // var secret = process.env.KALTURA_ADMIN_SECRET;
+  // var type = Kaltura.enums.KalturaSessionType.ADMIN;
+  //
+  // client.session.start(
+  //   function (ks) {
+  //     req.api.set('kaltura', {kalturaSession: ks});
+  //     next();
+  //   },
+  //   secret,
+  //   userId,
+  //   type,
+  //   partnerId,
+  //   expiry,
+  //   privileges);
+
+  client.user.login(function(data) {
+      var ks = (typeof data === 'string' ? data : 'no-valid-ks');
       req.api.set('kaltura', {kalturaSession: ks});
+
+      client.setKs(ks);
+
       next();
     },
-    secret,
+    partnerId,
     userId,
-    type,
-    accountID,
+    password,
     expiry,
     privileges);
+
 };
